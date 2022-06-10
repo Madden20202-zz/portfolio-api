44aba8ba36eec23beaf3048e0fd382da43b210ed
austin <business.mocksock@gmail.com>
2022-06-10 12:27:58 -0500

    fil in blueprint and json response

--- blueprints/personal_information.py
+++ blueprints/personal_information.py
@@ -1,4 +1,14 @@
 from flask import jsonify
-from flask import blueprints
+from flask import Blueprint
 
-# information needed: name address phone number github page
+personal_info_bp = Blueprint('personal', __name__)
+
+@personal_info_bp.route('/')
+
+def give_personal_info():
+    return jsonify{{
+        'name': 'Austin Martz',
+        'address': '407 W 6th Street Okmulgee OK 74447',
+        'phone_number': '539-286-3763',
+        'github_page': 'github.com/madden20202'
+    }}
