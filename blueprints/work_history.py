4c53171689380519e9ced285a1eb3a7a39296f74
austin <business.mocksock@gmail.com>
2022-06-10 17:14:48 -0500

    make basics of the work history file

--- blueprints/work_history.py
+++ blueprints/work_history.py
@@ -1,4 +1,17 @@
 from flask import jsonify
+from flask import Blueprint
 
 # Values needed: business name, work duration, job role
 # other possible values, work references, phone number
+
+work_history_bp = Blueprint('work_history', __name__)
+
+@work_history_bp.route('/')
+
+def get_work_history(): 
+    return 'I need to be able to give multiple values'
+
+class work_history: {
+
+    
+}
