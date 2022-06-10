071bfa539f44528e72727b13b6c183f0fb5b47fd
austin <business.mocksock@gmail.com>
2022-06-10 12:30:00 -0500

    fix jsonify issue and link

--- blueprints/personal_information.py
+++ blueprints/personal_information.py
@@ -6,9 +6,9 @@ personal_info_bp = Blueprint('personal', __name__)
 @personal_info_bp.route('/')
 
 def give_personal_info():
-    return jsonify{{
+    return jsonify({
         'name': 'Austin Martz',
         'address': '407 W 6th Street Okmulgee OK 74447',
         'phone_number': '539-286-3763',
-        'github_page': 'github.com/madden20202'
-    }}
+        'github_page': 'www.github.com/madden20202'
+    })
