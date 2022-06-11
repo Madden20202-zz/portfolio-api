181bcad34cc71c00c38cd0311dbc804cc0954bd2
austin <business.mocksock@gmail.com>
2022-06-11 05:07:25 -0500

    Got to reset and change gears

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 app.run(debug=True, port="localhost")
+
+# Data Table work
+
--- blueprints/work_history.py
+++ blueprints/work_history.py
@@ -8,6 +8,12 @@ work_history_bp = Blueprint('work_history', __name__)
 
 @work_history_bp.route('/')
 
-def get_work_history(): 
-    return 'I need to be able to give multiple values'
+def get_work_history():
+    return 'This will get the work history data'
 
+# So how I went about this was completely wrong
+# make data base first, pass down data from database 
+# jsonify it and send to angular
+
+# I honestly know better, I shoukdnt have gone for 
+# the hack solution but i foolishly did
